e4e43c9d8df84bf09877a5696aab55729d3ea8d3
Andre Muta <andremuta@gmail.com>
2025-08-01 15:40:09 -0300

    MigrationStartsHere

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,23 +4,23 @@ require "bundler/setup"
 require "kumi"
 require "pry"
 
-require "simplecov"
-SimpleCov.start do
-  add_filter "/spec/"
-  add_filter "/vendor/"
+# require "simplecov"
+# SimpleCov.start do
+#   add_filter "/spec/"
+#   add_filter "/vendor/"
 
-  add_group "Parser", "lib/kumi/ruby_parser"
-  add_group "Core", ["lib/kumi/schema.rb", "lib/kumi/types.rb", "lib/kumi/function_registry.rb"]
-  add_group "Analyzer", "lib/kumi/analyzer"
-  add_group "Compiler", "lib/kumi/compiler"
-  add_group "Syntax", "lib/kumi/syntax"
-  add_group "Input", "lib/kumi/input"
-  add_group "Domain", "lib/kumi/domain"
-  add_group "Text Parser", "lib/kumi/text_parser"
+#   add_group "Parser", "lib/kumi/ruby_parser"
+#   add_group "Core", ["lib/kumi/schema.rb", "lib/kumi/types.rb", "lib/kumi/function_registry.rb"]
+#   add_group "Analyzer", "lib/kumi/analyzer"
+#   add_group "Compiler", "lib/kumi/compiler"
+#   add_group "Syntax", "lib/kumi/syntax"
+#   add_group "Input", "lib/kumi/input"
+#   add_group "Domain", "lib/kumi/domain"
+#   add_group "Text Parser", "lib/kumi/text_parser"
 
-  minimum_coverage 0
-  track_files "lib/**/*.rb"
-end
+#   minimum_coverage 0
+#   track_files "lib/**/*.rb"
+# end
 
 Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
 
