17d62523ce0e940cae9e45dd0e97fe992e27ec2f
Andre Muta <andremuta@gmail.com>
2025-08-08 22:27:04 -0300

    Remove duplicated declaration of location

--- lib/kumi/syntax/node.rb
+++ lib/kumi/syntax/node.rb
@@ -3,7 +3,6 @@
 module Kumi
   module Syntax
     # A struct to hold standardized source location information.
-    Location = Struct.new(:file, :line, :column, keyword_init: true)
 
     # Base module included by all AST nodes to provide a standard
     # interface for accessing source location information..
