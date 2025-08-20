d3e22f2e5bf567b3019fc32a13ff048392e4caab
Andre Muta <andremuta@gmail.com>
2025-08-20 14:27:21 -0300

    More cleanup

--- lib/kumi/function_builder.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Minimal public builder used by app initializers to register custom functions.
-
-module Kumi
-  # Empty module to satisfy zeitwerk expectations
-  module FunctionBuilder
-  end
-end
