a40b66f22e027070783bdc4752267119b0cc4618
Trevor Nelson <trevor.nelson1@gmail.com>
2026-02-12 14:55:10 -0500

    Move AUTOLOADER into Kumi namespace

--- lib/kumi.rb
+++ lib/kumi.rb
@@ -4,42 +4,42 @@ require "zeitwerk"
 require "mutex_m"
 # require "pry" # COMMENT AFTER DEBUGGING
 
-AUTOLOADER = Zeitwerk::Loader.for_gem
-AUTOLOADER.ignore("#{__dir__}/kumi-cli")
-AUTOLOADER.ignore("#{__dir__}/kumi/dev/golden_schema_modules.rb")
-AUTOLOADER.inflector.inflect(
-  "assemble_irv2_pass" => "AssembleIRV2Pass",
-  "ast_emitter" => "ASTEmitter",
-  "attach_terminal_info_pass" => "AttachTerminalInfoPass",
-  "cgir" => "CGIR",
-  "global_cse_pass" => "GlobalCSEPass",
-  "ir_dependency_pass" => "IRDependencyPass",
-  "ir" => "IR",
-  "ir_dump" => "IRDump",
-  "ir_render" => "IRRender",
-  "ir_execution_schedule_pass" => "IRExecutionSchedulePass",
-  "irv2" => "IRV2",
-  "irv2_formatter" => "IRV2Formatter",
-  "lir" => "LIR",
-  "lir_ruby_codegen_pass" => "LIRRubyCodegenPass",
-  "lir_printer" => "LIRPrinter",
-  "local_cse_pass" => "LocalCSEPass",
-  "lower_to_ir_pass" => "LowerToIRPass",
-  "lower_to_lir_pass" => "LowerToLIRPass",
-  "lower_to_irv2_pass" => "LowerToIRV2Pass",
-  "load_input_cse" => "LoadInputCSE",
-  "nast" => "NAST",
-  "normalize_to_nast_pass" => "NormalizeToNASTPass",
-  "nast_dimensional_analyzer_pass" => "NASTDimensionalAnalyzerPass",
-  "nast_printer" => "NASTPrinter",
-  "snast_printer" => "SNASTPrinter",
-  "snast_pass" => "SNASTPass",
-  "ruby_ast" => "RubyAST",
-  "vm" => "VM"
-)
-AUTOLOADER.setup
-
 module Kumi
+  AUTOLOADER = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
+  AUTOLOADER.ignore("#{__dir__}/kumi-cli")
+  AUTOLOADER.ignore("#{__dir__}/kumi/dev/golden_schema_modules.rb")
+  AUTOLOADER.inflector.inflect(
+    "assemble_irv2_pass" => "AssembleIRV2Pass",
+    "ast_emitter" => "ASTEmitter",
+    "attach_terminal_info_pass" => "AttachTerminalInfoPass",
+    "cgir" => "CGIR",
+    "global_cse_pass" => "GlobalCSEPass",
+    "ir_dependency_pass" => "IRDependencyPass",
+    "ir" => "IR",
+    "ir_dump" => "IRDump",
+    "ir_render" => "IRRender",
+    "ir_execution_schedule_pass" => "IRExecutionSchedulePass",
+    "irv2" => "IRV2",
+    "irv2_formatter" => "IRV2Formatter",
+    "lir" => "LIR",
+    "lir_ruby_codegen_pass" => "LIRRubyCodegenPass",
+    "lir_printer" => "LIRPrinter",
+    "local_cse_pass" => "LocalCSEPass",
+    "lower_to_ir_pass" => "LowerToIRPass",
+    "lower_to_lir_pass" => "LowerToLIRPass",
+    "lower_to_irv2_pass" => "LowerToIRV2Pass",
+    "load_input_cse" => "LoadInputCSE",
+    "nast" => "NAST",
+    "normalize_to_nast_pass" => "NormalizeToNASTPass",
+    "nast_dimensional_analyzer_pass" => "NASTDimensionalAnalyzerPass",
+    "nast_printer" => "NASTPrinter",
+    "snast_printer" => "SNASTPrinter",
+    "snast_pass" => "SNASTPass",
+    "ruby_ast" => "RubyAST",
+    "vm" => "VM"
+  )
+  AUTOLOADER.setup
+
   # Provides access to the singleton configuration object.
   #
   # @return [Kumi::Configuration] the configuration instance
